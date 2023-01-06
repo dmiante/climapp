352beabe8bca481e802f5ff3a07a63a3445c9d01
damian <the.dmian@live.cl>
2023-01-05 21:17:10 -0300

    Change dynamic icon condition

--- src/components/Current/CurrentDay.jsx
+++ src/components/Current/CurrentDay.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import styles from './CurrentDay.module.css'
 import Location from '../../assets/location.svg'
 import Humidity from '../../assets/Humidity.png'
@@ -18,6 +19,56 @@ const MONTHS = {
   11: 'November',
   12: 'December'
 }
+export const WEATHER_CODES = {
+  1000: ['Sunny', 'Moon'],
+  1003: ['PartlyCloudy'],
+  1006: ['Cloudy'],
+  1009: ['Cloudy'],
+  1030: ['Haze', 'Fog'],
+  1063: ['RainCloud'],
+  1066: ['Snow'],
+  1069: ['Sleet'],
+  1072: ['IceSnow'],
+  1087: ['CloudThunder'],
+  1114: ['Snow'],
+  1117: ['Blizzard'],
+  1135: ['Haze', 'Fog'],
+  1147: ['Haze', 'Fog'],
+  1150: ['LightRain'],
+  1153: ['LightRain'],
+  1168: ['IceSnow'],
+  1171: ['IceSnow'],
+  1180: ['LightRain'],
+  1183: ['LightRain'],
+  1186: ['Rain'],
+  1189: ['Rain'],
+  1192: ['HeavyRain'],
+  1195: ['HeavyRain'],
+  1198: ['Hail'],
+  1201: ['Hail'],
+  1204: ['Sleet'],
+  1207: ['Sleet'],
+  1210: ['Snow'],
+  1213: ['Snow'],
+  1216: ['Snow'],
+  1219: ['Snow'],
+  1222: ['Blizzard'],
+  1225: ['Blizzard'],
+  1237: ['Hail'],
+  1240: ['LightRain'],
+  1243: ['HeavyRain'],
+  1246: ['HeavyRain'],
+  1249: ['Sleet'],
+  1252: ['Sleet'],
+  1255: ['Snow'],
+  1258: ['Blizzard'],
+  1261: ['Hail'],
+  1264: ['Hail'],
+  1273: ['RainCloud'],
+  1276: ['ThunderStorm'],
+  1279: ['Snow'],
+  1282: ['Blizzard']
+}
 export default function CurrentDay ({ weather }) {
   const localtime = weather.localtime
   const time = localtime?.slice(-5)
@@ -25,6 +76,19 @@ export default function CurrentDay ({ weather }) {
   const month = localtime?.slice(5, 7)
   const day = localtime?.slice(8, 10)
   const date = ` ${day} ${MONTHS[month]} ${year}`
+
+  const fileName = WEATHER_CODES[weather?.code]?.[
+    ((weather?.code === 1000 || weather?.code === 1030) && weather?.isDay) === 0 ? 1 : 0
+  ]
+  const [icon, setIcon] = useState('')
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      const response = await import('../../assets/ConditionIcons/' + fileName + '.png')
+      setIcon(response.default)
+    }
+    fetchImage()
+  }, [fileName])
 
   return (
     <div className={styles.currentDay}>
@@ -34,7 +98,7 @@ export default function CurrentDay ({ weather }) {
           <h3 className={styles.city}>{`${weather.name}, ${weather.country}`}</h3>
         </div>
         <h3 className={styles.localtime}>{`${time} - ${date}`}</h3>
-        <img className={styles.icon} src={weather.icon} />
+        <img className={styles.icon} src={icon} />
         <h1 className={styles.temperature}>{`${weather.tempNow}°`}</h1>
         <h2 className={styles.condition}>{weather.conditionText}</h2>
       </section>
