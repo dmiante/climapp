7d8579d8a22d048c6636af87801a89018b4d3bd7
damian <the.dmian@live.cl>
2023-01-09 23:06:26 -0300

    ADD simple loading spinner

---
+++ src/components/Loading/Nprogress.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.css'
+
+export default function Spinner () {
+  return (
+    <div class='spinner'>
+      <div class='double-bounce1' />
+      <div class='double-bounce2' />
+    </div>
+  )
+}
