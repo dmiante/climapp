8db37cdd76f6c910e03395259627e5be7f7247ca
damian <the.dmian@live.cl>
2023-01-18 19:42:56 -0300

    ADD vercel and delete gh-pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -31,7 +31,7 @@ export default function Home () {
   }
 
   const { image } = useImage(fileName)
-  const backgroundOne = image?.slice(0, 20) + 'bg' + image?.slice(20)
+  const backgroundOne = image?.slice(0, 12) + 'bg' + image?.slice(12)
   console.log(backgroundOne)
 
   const backgroundStyle = {
